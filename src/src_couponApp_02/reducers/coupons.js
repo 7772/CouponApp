f64a71cae922f582e798788b29e2cad703562456
LandonPark <sbshz85@gmail.com>
2017-10-09 01:42:16 -0700

    MyCoupon 리듀서 추가

--- src/src_couponApp_02/reducers/coupons.js
+++ src/src_couponApp_02/reducers/coupons.js
@@ -1,6 +1,8 @@
-import { ADD_COUPON, LOAD_DATA } from '../actions/types';
+import * as actions from '../actions/types';
 import Coupon from './../data/Coupon';
 import { writeCoupons } from './../storage/coupons';
+
+import _ from 'lodash';
 
 function saveCoupons(state) {
   writeCoupons(state);
@@ -9,14 +11,31 @@ function saveCoupons(state) {
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case LOAD_DATA:
+
+    case actions.LOAD_DATA:
       return action.data;
-    case ADD_COUPON:
+
+    case actions.ADD_COUPON:
       let newState = state.concat(action.data);
       saveCoupons(newState);
       return newState;
+
+    case actions.MYCOUPON:
+      let index = _.findIndex(state, (key) => key.id === action.id );
+      if(index === -1) {
+        return state;
+      }
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, state[index], {
+          completed: true
+        }),
+        ...state.slice(index + 1)
+      ];
+      
   }
   return state;
+
 };
 
 
